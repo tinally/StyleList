1497b03f390d1f21da4ac5c1baa9fb41944c7e7d
kch3coo <xia0weimo1999@gmail.com>
2020-01-18 11:28:19 -0500

    kk

--- stylelist/src/App.js
+++ stylelist/src/App.js
@@ -10,6 +10,7 @@ import {
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
 import ProductCard from './ProductCard';
+import Figure from './components/Figure/Figure'
 
 
 function App() {
@@ -19,68 +20,21 @@ function App() {
         <Navbar.Brand href="/">StyleList</Navbar.Brand>
         <Nav className="mr-auto">
           <Nav.Link href="products">Products</Nav.Link>
+          <Nav.Link href="figure">Figure</Nav.Link>
         </Nav>
       </Navbar>
       <Router>
-        <div className="App">
+        {/* <div className="App">
           <Switch>
             <Route path="/products">
               <ProductCard />
             </Route>
           </Switch>
-        </div>
+        </div> */}
+        <Route path="/products" component={ProductCard} />
+        <Route path="/figure" component={Figure} />
       </Router>
-    <div className="App">
-      Hello
-      <section class="bg-white py-md-5 separator-bottom">
-        <div class="container">
-          <div class="row justify-content-center">
-            <div class="col-md-6 text-center">
-              <span class="eyebrow d-block mb-1 text-primary">
-                Our Creative Team
-              </span>
-            </div>
-          </div>
-          <div class="row gutter-0">
-            <div class="col-md-4" data-aos="fade-up" data-aos-delay="100">
-              <figure class="user">
-                <a href="" class="user-photo">
-                  <img src="kevin.png" alt="Avatar" />
-                </a>
-                <figcaption class="user-caption text-center">
-                  <h4>Kevin</h4>
-                  <span>MAT137, PHY131, MAT187</span>
-                  <ul class="socials">
-                    <section class="text-left text-white pr-md-2">
-                      Master in Mechanical Engineering at the University of
-                      Toronto Research focus: Electrochemical and fuel cells and
-                      fluid mechanics 3-year Research Assistant
-                    </section>
-                  </ul>
-                </figcaption>
-              </figure>
-            </div>
-            <div class="col-md-4" data-aos="fade-up" data-aos-delay="200">
-              <figure class="user">
-                <a href="" class="user-photo">
-                  <img
-                    src="../../assets/img/instructors/Nancy.png"
-                    alt="Avatar"
-                  />
-                </a>
-                <figcaption class="user-caption text-center">
-                  <h4>Nancy</h4>
-                  <span>STA220, CHM135</span>
-                  <ul class="socials">
-                    <section class="text-left text-white pr-md-2"></section>
-                  </ul>
-                </figcaption>
-              </figure>
-            </div>
-          </div>
-        </div>
-      </section>
-    </div>
+      
     </>
   );
   
---
+++ stylelist/src/components/Figure/Figure.js
@@ -0,0 +1,57 @@
+import React, { Component } from "react";
+
+export default class Figure extends Component {
+  render() {
+    return (
+      <div>
+          <div class="container">
+            <div class="row justify-content-center">
+              <div class="col-md-6 text-center">
+                <span class="eyebrow d-block mb-1 text-primary">
+                  Our Creative Team
+                </span>
+              </div>
+            </div>
+            <div class="row gutter-0">
+              <div class="col-md-4" data-aos="fade-up" data-aos-delay="100">
+                <figure class="user">
+                  <a href="" class="user-photo">
+                    <img src="kevin.png" alt="Avatar" />
+                  </a>
+                  <figcaption class="user-caption text-center">
+                    <h4>Kevin</h4>
+                    <span>MAT137, PHY131, MAT187</span>
+                    <ul class="socials">
+                      <section class="text-left text-white pr-md-2">
+                        Master in Mechanical Engineering at the University of
+                        Toronto Research focus: Electrochemical and fuel cells
+                        and fluid mechanics 3-year Research Assistant
+                      </section>
+                    </ul>
+                  </figcaption>
+                </figure>
+              </div>
+              <div class="col-md-4" data-aos="fade-up" data-aos-delay="200">
+                <figure class="user">
+                  <a href="" class="user-photo">
+                    <img
+                      src="../../assets/img/instructors/Nancy.png"
+                      alt="Avatar"
+                    />
+                  </a>
+                  <figcaption class="user-caption text-center">
+                    <h4>Nancy</h4>
+                    <span>STA220, CHM135</span>
+                    <ul class="socials">
+                      <section class="text-left text-white pr-md-2"></section>
+                    </ul>
+                  </figcaption>
+                </figure>
+              </div>
+            </div>
+          </div>
+
+      </div>
+    );
+  }
+}
