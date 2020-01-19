d226052740e29942f92ea7be8f2f0f4c11535e35
kch3coo <xia0weimo1999@gmail.com>
2020-01-19 07:10:20 -0500

    deisgn

---
+++ stylelist/src/components/Nav/TopNavbar.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
+import "./TopNavbar.css"
+export default class TopNavbar extends Component {
+  render() {
+    return (
+      <div>
+        <Navbar bg="light" id="topNav" expand="lg">
+          <Navbar.Brand href="/">
+            <img src="/logo.png" style={{height: "55px"}}/>
+            </Navbar.Brand>
+
+        </Navbar>
+      </div>
+    );
+  }
+}
